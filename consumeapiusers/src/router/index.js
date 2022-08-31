ada93c5821abc30151c52a04175c653294cf0c06
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-08-31 11:30:29 -0300

    Auth access by Backend

--- consumeapiusers/src/router/index.js
+++ consumeapiusers/src/router/index.js
@@ -3,6 +3,34 @@ import HomeView from '../views/HomeView.vue'
 import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import Users from '../views/Users.vue'
+import axios from 'axios'
+
+function AdminAuth(to, from, next) {
+
+  //Essa lógica pode ser burlada pelo console do nav, podendo setar qualquer valor no token do localstorage e entao entrar no sistema sem ter conta com permissão.
+  if(localStorage.getItem('token') != undefined){
+
+    const req = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    }
+    console.log(req)
+
+    //Quando for post deve passar um dado -> {} antes do req
+    axios.post('http://localhost:3333/validate', {}, req).then(res => {
+      console.log(res)
+      next()
+    }).catch(err => {
+
+      //Mensagem de erro caso não tenha permissão de acesso.
+      console.log(err.response.data)
+      next('/login')
+    })
+  } else {
+    next('/login')
+  }
+}
 
 const routes = [
   {
@@ -24,15 +52,7 @@ const routes = [
     path: '/admin/users',
     name: 'Users',
     component: Users,
-    beforeEnter: (to, from, next) => {
-
-      //Essa lógica pode ser burlada pelo console do nav, podendo setar qualquer valor no token do localstorage e entao entrar no sistema sem ter conta com permissão.
-      if(localStorage.getItem('token') != undefined){
-        next();
-      } else {
-        next('/login')
-      }
-    }
+    beforeEnter: AdminAuth
   },
   {
     path: '/about',
